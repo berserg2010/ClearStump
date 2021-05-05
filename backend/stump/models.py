c78958a1c118e523e1ea2c46eb5eef916a32ff52
Alnipet <a777petrov@gmail.com>
2021-05-06 00:23:57 +0300

    created first models in models.py

--- backend/stump/models.py
+++ backend/stump/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+
+class ServiceCategory(models.Model):
+
+    title = models.CharField(
+        max_length=15,
+        verbose_name='Наименование категории'
+    )
+
+    description = models.TextField(
+        max_length=150,
+        verbose_name='Описание категории',
+        blank=True,
+    )
+
+    image = models.ImageField(
+        blank=True,
+    )
+
+
+class ServiceCard(models.Model):
+
+    title = models.CharField(
+        max_length=15,
+        verbose_name='Наименование услуги'
+    )
+
+    description = models.TextField(
+        max_length=150,
+        verbose_name='Описание услуги',
+        blank=True,
+    )
+
+    category = models.ForeignKey(
+        ServiceCategory,
+        verbose_name='Категория услуги',
+        on_delete=models.CASCADE,
+    )
+
+    image = models.ImageField(
+        blank=True,
+    )
+
+    is_active = models.BooleanField()
+
+class SettingsSite(models.Model):
+
+    address = models.CharField(
+        max_length=50,
+        verbose_name='Адрес'
+    )
+
+    email = models.EmailField()
+
+    phone = models.CharField(
+        max_length=50,
+        verbose_name='Номер телефона'
+    )
+
+    is_active = models.BooleanField()

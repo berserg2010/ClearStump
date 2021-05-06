ddeae7fb73c6549d552d5d9c8057af8f8a8968fa
Alnipet <a777petrov@gmail.com>
2021-05-06 22:50:57 +0300

    fixed first models in models.py

--- backend/stump/models.py
+++ backend/stump/models.py
@@ -4,58 +4,64 @@ from django.db import models
 class ServiceCategory(models.Model):
 
     title = models.CharField(
-        max_length=15,
+        max_length=150,
         verbose_name='Наименование категории'
     )
 
     description = models.TextField(
-        max_length=150,
+        max_length=250,
         verbose_name='Описание категории',
         blank=True,
     )
 
     image = models.ImageField(
+        upload_to='img/',
+        verbose_name='изображение',
         blank=True,
+        null=True,
     )
 
 
 class ServiceCard(models.Model):
 
     title = models.CharField(
-        max_length=15,
-        verbose_name='Наименование услуги'
+        max_length=150,
+        verbose_name='наименование услуги',
     )
 
     description = models.TextField(
-        max_length=150,
-        verbose_name='Описание услуги',
+        max_length=250,
+        verbose_name='описание услуги',
         blank=True,
     )
 
     category = models.ForeignKey(
         ServiceCategory,
-        verbose_name='Категория услуги',
+        verbose_name='категория услуги',
         on_delete=models.CASCADE,
     )
 
     image = models.ImageField(
+        upload_to='img/',
+        verbose_name='изображение',
         blank=True,
+        null=True,
     )
 
     is_active = models.BooleanField()
+
 
 class SettingsSite(models.Model):
 
     address = models.CharField(
         max_length=50,
-        verbose_name='Адрес'
+        verbose_name='адрес'
+    )
+    phone = models.CharField(
+        max_length=50,
+        verbose_name='номер телефона'
     )
 
     email = models.EmailField()
-
-    phone = models.CharField(
-        max_length=50,
-        verbose_name='Номер телефона'
-    )
 
     is_active = models.BooleanField()

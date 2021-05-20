2ef1951ef06c748c0f44fcbf2a09168e7b962336
Alnipet <a777petrov@gmail.com>
2021-05-21 00:29:43 +0300

    create navbar, first page #5

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
   <main>
+    <Navbar />
     <Nuxt />
   </main>
 </template>
+
+<script>
+import Navbar from '../components/Navbar'
+export default {
+  comments: {
+    Navbar
+  }
+}
+</script>
--- frontend/pages/Swipe.vue
+++ frontend/pages/Swipe.vue
@@ -1,6 +1,29 @@
 <template>
   <swiper class="swiper" :options="swiperOption" style="height:100vh">
-    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>
+      <section class="home">
+            <div class="container">
+                <div class="home_inner">
+                    <div class="home_inner-content">
+                        <div class="home_title">
+                            <h1>Изготовление на заказ</h1>
+                        </div>
+                        <div class="home_text">
+                            <h2>И сказал Бог: да будут светила на тверди небесной для отделения дня от ночи, и для знамений, и времен, и дней, и годов; и да будут они светильниками на тверди небесной, чтобы светить на землю. И стало так.
+                                И создал Бог два светила великие: светило большее, для управления днем, и светило меньшее, для управления ночью, и звезды;
+                                и поставил их Бог на тверди небесной, чтобы светить на землю, и управлять днем и ночью, и отделять свет от тьмы. И увидел Бог, что это хорошо.
+                                И был вечер, и было утро: день четвёртый.</h2>
+                        </div>
+                        <div class="home_btn">
+                            <a href='#'>Оставить заявку</a>
+                        </div>
+                    </div>
+                    <div class="home_img">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </swiper-slide>
     <swiper-slide>Slide 2</swiper-slide>
     <swiper-slide>Slide 3</swiper-slide>
     <swiper-slide>Slide 4</swiper-slide>

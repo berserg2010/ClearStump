f1ffa49e9b1a2e356821e7b6198983202b4172ca
Sergei Zakamskov <berserg2010@gmail.com>
2022-05-03 00:12:48 +0300

    Переход на node 16, добавление изображений, создание разделов в галерее #10

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [ { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' } ],
   },
 
   // gh-pages
@@ -21,10 +21,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/scss/main.scss', 'element-ui/lib/theme-chalk/index.css'],
+  css: [ '~/assets/scss/main.scss', 'element-ui/lib/theme-chalk/index.css' ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/element-ui'],
+  plugins: [ '@/plugins/element-ui' ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -48,6 +48,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: [/^element-ui/],
+    transpile: [ /^element-ui/ ],
   },
 };
